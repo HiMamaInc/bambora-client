9724c2d00ec1eb53cd160dffdf5edd66663ea99b
Harry Lewis <harry.lewis@queensu.ca>
2019-10-28 13:59:22 -0400

    Add class-level docs for easier console debugging

--- lib/bambora/v1/payment_resource.rb
+++ lib/bambora/v1/payment_resource.rb
@@ -5,6 +5,12 @@ module Bambora
     class PaymentResource
       CREDIT_CARD_ID_DEFAULT = 1
 
+      # Summary: Create and modify payments.
+      # Note: The link below links to all apis includding profiles and tokenization. There aren't great docs explaining
+      #       the /payments endpoints alone.
+      # Docs: https://dev.na.bambora.com/docs/references/payment_APIs/
+      #       https://dev.na.bambora.com/docs/references/payment_SDKs/take_payments/?shell#
+      # Endpoint: https://api.na.bambora.com/v1/payments
       def initialize(client:)
         @client = client
         @sub_path = '/v1/payments'
