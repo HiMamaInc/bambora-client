beadf65e9b5c3c4908de7685adfa6478ad7a78db
Harry Lewis <harry.lewis@queensu.ca>
2019-10-27 22:02:58 -0400

    Implement #make_payment

--- lib/bambora/v1/payment_resource.rb
+++ lib/bambora/v1/payment_resource.rb
@@ -9,9 +9,13 @@ module Bambora
       # Docs: https://dev.na.bambora.com/docs/references/payment_APIs/
       #       https://dev.na.bambora.com/docs/references/payment_SDKs/take_payments/?shell#
       # Endpoint: https://api.na.bambora.com/v1/payments
-      def initialize(client:, sub_path:)
+      def initialize(client:)
         @client = client
-        @sub_path = sub_path
+        @sub_path = '/v1/payments'
+      end
+
+      def make_payment(payment_data)
+        @client.request(method: :post, path: @sub_path, body: payment_data)
       end
     end
   end
--- spec/bambora/v1/payment_resource_spec.rb
+++ spec/bambora/v1/payment_resource_spec.rb
@@ -38,9 +38,41 @@ module Bambora
         )
       end
 
-      subject { described_class.new(client: client, sub_path: '/v1/payments') }
+      subject { described_class.new(client: client) }
 
-      pending
+      describe '#make_payment' do
+        before(:each) do
+          stub_request(:post, "#{base_url}/v1/payments").with(
+            body: data.to_json.to_s,
+            headers: headers,
+          ).to_return(body: response_body.to_json.to_s)
+        end
+
+        let(:data) do
+          {
+            amount: 50,
+            payment_method: 'card',
+            card: {
+              name: 'Hup Podling',
+              number: '4030000010001234',
+              expiry_month: '12',
+              expiry_year: '23',
+              cvd: '123',
+            },
+          }
+        end
+
+        it "POST's to the Bambora API" do
+          subject.make_payment(data)
+
+          expect(
+            a_request(:post, "#{base_url}/v1/payments").with(
+              body: data.to_json.to_s,
+              headers: headers,
+            ),
+          ).to have_been_made.once
+        end
+      end
     end
   end
 end
