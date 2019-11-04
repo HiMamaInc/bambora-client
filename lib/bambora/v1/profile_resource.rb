d225624ce043d53210a26f4f85f3e58098693f23
Harry Lewis <harry.lewis@queensu.ca>
2019-11-04 11:55:27 -0500

    Add rdoc to Bambora::V1::ProfileResource#create

--- lib/bambora/v1/profile_resource.rb
+++ lib/bambora/v1/profile_resource.rb
@@ -8,6 +8,35 @@ module Bambora
         @sub_path = '/v1/profiles'
       end
 
+      # Create a Bambora payment profile.
+      #
+      # @example
+      #
+      #   client = Bambora::JSONClient(base_url: '...', api_key: '...', merchant_id: '...')
+      #   profiles = Bambora::V1::ProfileResource(client: client)
+      #   data = {
+      #    language: 'en',
+      #     card: {
+      #       name: 'Hup Podling',
+      #       number: '4030000010001234',
+      #       expiry_month: '12',
+      #       expiry_year: '23',
+      #       cvd: '123',
+      #     },
+      #   }
+      #
+      #   profiles.create(data)
+      #   # => {
+      #   #      :code => 1,
+      #   #      :message => "Operation Successful",
+      #   #      :customer_code => "02355E2e58Bf488EAB4EaFAD7083dB6A",
+      #   #    }
+      #
+      # @param card_data [Hash] All information relevant to making a payment.
+      #
+      # @see https://dev.na.bambora.com/docs/guides/payment_profiles
+      #
+      # @return [Hash] Indicating success or failure of the operation.
       def create(card_data)
         @client.request(method: :post, path: @sub_path, body: card_data)
       end
@@ -18,7 +47,7 @@ module Bambora
       #
       #   client = Bambora::JSONClient(base_url: '...', api_key: '...', merchant_id: '...')
       #   profiles = Bambora::V1::ProfileResource(client: client)
-      #   customer_code: '02355E2e58Bf488EAB4EaFAD7083dB6A'
+      #   customer_code = '02355E2e58Bf488EAB4EaFAD7083dB6A'
       #
       #   profiles.delete(customer_code: customer_code)
       #   # => {
