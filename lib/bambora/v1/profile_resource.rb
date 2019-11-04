cf942997be7c777c530ad47a0c4bba554a45134a
Harry Lewis <harry.lewis@queensu.ca>
2019-11-04 11:42:59 -0500

    Add rdoc to Bambora::V1::ProfileResource#delete

--- lib/bambora/v1/profile_resource.rb
+++ lib/bambora/v1/profile_resource.rb
@@ -12,6 +12,24 @@ module Bambora
         @client.request(method: :post, path: @sub_path, body: card_data)
       end
 
+      # Delete a Bambora payment profile given a customer code.
+      #
+      # @example
+      #
+      #   client = Bambora::JSONClient(base_url: '...', api_key: '...', merchant_id: '...')
+      #   profiles = Bambora::V1::ProfileResource(client: client)
+      #   customer_code: '02355E2e58Bf488EAB4EaFAD7083dB6A'
+      #
+      #   profiles.delete(customer_code: customer_code)
+      #   # => {
+      #   #      :code => 1,
+      #   #      :message => "Operation Successful",
+      #   #      :customer_code => "02355E2e58Bf488EAB4EaFAD7083dB6A",
+      #   #    }
+      #
+      # @param customer_code [String] A unique identifier for the associated payment profile.
+      #
+      # @return [Hash] Indicating success or failure of the operation.
       def delete(customer_code:)
         @client.request(method: :delete, path: "#{@sub_path}/#{customer_code}")
       end
