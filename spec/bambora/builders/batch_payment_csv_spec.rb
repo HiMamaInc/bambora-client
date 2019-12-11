3ffe637d36909245385d906a606a308ace22c1a5
Cassidy K <hello@cassidy.codes>
2019-12-11 12:10:07 -0500

    Adding CSV builder

---
+++ spec/bambora/builders/batch_payment_csv_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Bambora
+  module Builders
+    describe BatchPaymentCSV do
+      describe '#build' do
+        let(:transactions) do
+          [
+            {
+              super_type: 'E',
+              transaction_type: 'D',
+              institution_number: 12_345,
+              transit_number: 123,
+              account_number: 123_456_789,
+              amount: 10_000,
+              reference_nubmer: 1234,
+              reccipient_name: 'Hup Podling',
+              customer_code: '02355E2e58Bf488EAB4EaFAD7083dB6A',
+              dynamic_description: 'The Skeksis',
+            },
+          ]
+        end
+
+        let(:csv_string) do
+          "E,D,12345,123,123456789,10000,1234,Hup Podling,02355E2e58Bf488EAB4EaFAD7083dB6A,The Skeksis\n"
+        end
+
+        subject { described_class.build(transactions) }
+
+        it { is_expected.to eq(csv_string) }
+      end
+    end
+  end
+end
