f0a2ae59be3e1a334942f0d6239d35497675964d
Ed Lui <33198+erawk@users.noreply.github.com>
2020-04-14 16:05:20 -0400

    Bugfix: Handle null record from Bambora Batch Bank Report queries (#35)
    * Bugfix: Handle null record from Bambora Batch Bank Report queries
    
    * Simplify guard method and update help message

--- lib/bambora/bank/batch_report_resource.rb
+++ lib/bambora/bank/batch_report_resource.rb
@@ -61,9 +61,13 @@ module Bambora
       private
 
       def add_messages_to_response(response)
+        # bambora can return null or empty record results, don't decorate with messages
+        return response if response.dig(:response, :record).nil?
+
         response.dig(:response, :record).map! do |record|
           record.merge!(messages: record[:messageId].split(',').map { |id| MESSAGES[id] })
         end
+
         response
       end
 
