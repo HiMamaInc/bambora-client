e9300116c78fcc935377f3054511c2f12edd6993
Harry Lewis <harry.lewis@queensu.ca>
2024-03-06 22:57:39 -0500

    Add new API method, Bambora::V1::Reports::Merchants.get

--- lib/bambora/v1/reports/merchants.rb
+++ lib/bambora/v1/reports/merchants.rb
@@ -4,6 +4,14 @@ module Bambora
   module V1
     module Reports
       class Merchants
+        def self.get(merchant_id, options = {})
+          credentials = options[:credentials]
+
+          Bambora::Rest::JSONClient
+            .new(base_url: 'https://api.na.bambora.com', merchant_id: credentials.merchant_id)
+            .get(path: "/v1/reports/merchants/#{merchant_id}", api_key: credentials.reporting_passcode)
+        end
+
         def self.get_all(options = {})
           credentials = options[:credentials]
 
--- spec/bambora/v1/reports/merchants_spec.rb
+++ spec/bambora/v1/reports/merchants_spec.rb
@@ -1,6 +1,93 @@
 # frozen_string_literal: true
 
 RSpec.describe Bambora::V1::Reports::Merchants do
+  describe '.get' do
+    it 'returns data from GET /v1/reports/merchants/:merchant_id' do
+      WebMock
+        .stub_request(:get, 'https://api.na.bambora.com/v1/reports/merchants/372110001')
+        .with(
+          headers: {
+            'Authorization' => 'Passcode MzcyMTEwMDAwOjI4ODQwQ0VCOUg5RDNERkMyNEE0NDVCNzQ2MUZEOEZH',
+            'Content-Type' => 'application/json',
+          },
+        )
+        .to_return(
+          headers: {
+            'Content-Type' => 'application/json',
+          },
+          body: {
+            data: [
+              [
+                {
+                  merchant_id: 372_110_001,
+                  merchant_name: 'ABC Business',
+                  website: '',
+                  address: {
+                    street_address: '700 Main Street',
+                    province: 'New York',
+                    country: 'United States',
+                    postal_code: '82210',
+                  },
+                  merchant_status: {
+                    status: 'Active',
+                    state: 'Live',
+                    live_date: '2024-01-01',
+                    created_date: '2024-01-01',
+                    authorized_date: '2024-01-05',
+                    temp_disabled_date: '0001-01-01',
+                    last_enabled_date: '0001-01-01',
+                    disabled_date: '0001-01-01',
+                  },
+                  processor: {
+                    processor_name: 'First Data',
+                    currency: 'USD',
+                  },
+                  batch: {
+                    batch_limit: 100_000.0,
+                    batch_line_limit: 100_000.0,
+                    eft_credit_lag: 3,
+                  },
+                  settlement: {
+                    credit_lag: 0,
+                  },
+                  cards: {
+                    mastercard_enabled: false,
+                    visa_enabled: false,
+                    amex_enabled: false,
+                    discover_enabled: false,
+                    diners_enabled: false,
+                    jcb_enabled: false,
+                    mastercard_debit_enabled: false,
+                    visa_debit_enabled: false,
+                  },
+                  features: {
+                    checkout_enabled: true,
+                    payment_profile_enabled: false,
+                    recurring_billing_enabled: true,
+                    credit_card_batch_enabled: false,
+                    eft_ach_enabled: true,
+                    interac_online_enabled: false,
+                    visa_src_enabled: false,
+                  },
+                },
+              ],
+            ],
+          }.to_json,
+        )
+
+      credentials = Bambora::Credentials.new(
+        merchant_id: '372110000',
+        reporting_passcode: '28840CEB9H9D3DFC24A445B7461FD8FG',
+      )
+
+      merchants = described_class.get(372_110_001, credentials: credentials)
+
+      expect(merchants[:data][0].length).to eq(1)
+      expect(merchants[:data][0][0][:merchant_id]).to eq(372_110_001)
+      expect(merchants[:data][0][0][:merchant_name]).to eq('ABC Business')
+    end
+  end
+
   describe '.get_all' do
     it 'returns data from GET /v1/reports/merchants' do
       WebMock
